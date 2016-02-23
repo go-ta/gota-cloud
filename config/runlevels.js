8054c39afee2c3f9031ceae02b391eb7a9bda086
andreasabilio <andreas@abilio.dk>
2016-02-23 16:21:38 +0100

    Removed UID from runlevels config

--- config/runlevels.js
+++ config/runlevels.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var fs            = require('fs');
-var path          = require('path');
-var child_process = require('child_process');
+//var fs            = require('fs');
+//var path          = require('path');
+//var child_process = require('child_process');
 
 module.exports = {
   resource: {
     path: './lib/runlevel.js',
     args: [1],
     options: {
-      uid: 1
+      //uid: 1
     }
   }
 };
